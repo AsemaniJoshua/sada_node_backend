1b0ad226949a06bada622505cbf38c074ec74a43
ASEMANI JOSHUA <145305697+AsemaniJoshua@users.noreply.github.com>
2026-04-22 17:29:59 +0000

    feat: implement server setup with Express and CORS middleware

---
+++ server.js
@@ -0,0 +1,37 @@
+// Importing necessary modules and files
+import express from "express";
+import cors from "cors";
+import { customError } from "./utils/error/custom_error";
+
+
+// Initializing the express application
+const app = express();
+
+// Setting the port for the server to listen on
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON data from the request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to enable CORS (Cross-Origin Resource Sharing)
+app.use(cors({
+    origin: "*",
+    methods: ["*"],
+    allowedHeaders: ["*"],
+    credentials: true
+}));
+
+
+// Importing routes
+
+
+
+// Error handling middleware
+app.use(customError);
+
+
+// Starting the server and listening on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
